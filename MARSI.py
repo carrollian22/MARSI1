b84b5141a6f3dc0f26b57c9345a0fce46e29919a
carrollian22 <ic.analytics22@gmail.com>
2024-11-18 22:48:17 -0500

    Update MARSI.py

--- MARSI.py
+++ MARSI.py
@@ -1,7 +1,6 @@
 import os
 import telegram
 from tvDatafeed import TvDatafeed, Interval
-import yfinance as yf
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
